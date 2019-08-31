068376cc7153b1ef4201887059fd2c5e43f3cd83
Rasmus.Iila <rasmus.iila@ttu.ee>
2019-08-31 19:31:59 +0300

    Persons table added, some routing to add a new person and to retrieve all the sectors

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    <title>SectorApp</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,49 @@
 |
 */
 
+use App\Person;
+use App\Sector;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+
 Route::get('/', function () {
-    return view('broken');
+    $sectors = Sector::orderBy('created_at', 'asc')->get();
+    // TODO: get all sectors
+    return view('broken', [
+        'sectors' => $sectors
+    ]);
+});
+
+/**
+ * Add A New Person
+ */
+Route::post('/person', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    // TODO: maybe create a service file for this
+    $person = new Person;
+    $person->name = $request->name;
+    $person->sectors = $request->sectors;
+    $person->terms = $request->has('terms');
+
+    $person->save();
+
+    return redirect('/'); // TODO: return with the existing data
+});
+
+/**
+ * Update An Existing Person
+ */
+Route::post('/person/{id}', function ($id) {
+    //
 });
